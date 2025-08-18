482ceedfaf771807d588700c6a0bd9da55dcf208
saurabhb-dev <saurabh.banawar@gmail.com>
2025-08-18 18:27:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, pipeline.... pqrs - deployed again and againz!!!');
+    res.send('Hello, pipeline.... pqrs - deployed again and againzz!!!');
 });
 
 app.listen(PORT, () => {
